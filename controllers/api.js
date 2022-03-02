bad8909875d89ac43e30837db7eee998d870d8ce
Jazmin-Ewers <jazminewers11@gmail.com>
2022-03-02 10:21:51 -0500

    Add team html page to showcase attendees on specific teams

--- controllers/api.js
+++ controllers/api.js
@@ -1,10 +1,16 @@
 const Attendee = require('../models/attendee');
 
 module.exports = {
-    index
+    index,
+    showTeam
 };
 
 async function index(req, res) {
     const attendees = await Attendee.find({});
     res.render('api/index', { attendees });
+}
+
+async function showTeam(req, res) {
+    const team_members = await Attendee.find({team: 1 });
+    res.render('api/team', { team_members });
 }
--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,8 @@ const apiCtrl = require('../controllers/api');
 // GET '/api' all the attendees
 router.get('/', apiCtrl.index);
 
+// Get '/api/team/:id show all attendees on a certain team
+router.get('/team/:id', apiCtrl.showTeam);
+
+
 module.exports = router;
